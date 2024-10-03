adbfd561df7725cc43a2d914cfaab09661aeb815
medammadhuri <51746428+medammadhuri@users.noreply.github.com>
2024-10-03 19:03:56 +0530

    Update check.py

--- Dags/check.py
+++ Dags/check.py
@@ -1,13 +1,25 @@
 from datetime import datetime
 from airflow import DAG
-from airflow.operators.python_operator import PythonOperator
+from airflow.operators.python import PythonOperator
 from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 from mail import send_email_on_failure, send_email_on_retry, send_email_on_success
 
-def start_time():
+def start_time(**kwargs):
     # just print the time    
     start_time = datetime.now()
     print(start_time)
+    ti = kwargs['ti']
+    ti.xcom_push(key='start_time', value=start_time)
+    print('xcom_variable', start_time)
+    return start_time
+    
+def end_time(**kwargs):
+    # just print the time    
+    end = datetime.now()
+    print(end)
+    ti = kwargs['ti']
+    check = kwargs['dag_run'].conf.get('start_time')
+    print('xcom_variable', check)
 
 default_args = {
     'owner': 'airflow',
@@ -26,16 +38,17 @@ with DAG(
     start_task = PythonOperator(
         task_id='start_task',
         python_callable=start_time,
-        on_failure_callback=lambda context: send_email_on_failure(context, 'start1', 'start_task'),
+        on_failure_callback=lambda context: send_email_on_retry(context, 'start1', 'start_task'),
         on_retry_callback=lambda context: send_email_on_retry(context, 'start1', 'start_task'),
         on_success_callback=lambda context: send_email_on_success(context, 'start1', 'start_task'),
     )
 
     # Trigger the 'end1' DAG after the start_task runs successfully
     trigger_end1 = TriggerDagRunOperator(
-        task_id='end1',
+        task_id='trigger_end1',
         trigger_dag_id='end1',
-        on_success_callback=lambda context: send_email_on_success(context, 'start1', "trigger"),
+        conf={'start_time': '{{ ti.xcom_pull(task_ids="start_task") }}'},
+        on_success_callback=lambda context: send_email_on_retry(context, 'start1', "trigger"),
         on_failure_callback=lambda context: send_email_on_failure(context, 'start1', "trigger"),
         on_retry_callback=lambda context: send_email_on_retry(context, 'start1', "trigger"),
     )
@@ -52,10 +65,10 @@ with DAG(
 
     check = PythonOperator(
         task_id='check',
-        python_callable=start_time,
+        python_callable=end_time,
         on_failure_callback=lambda context: send_email_on_failure(context, 'end1', 'check'),
         on_retry_callback=lambda context: send_email_on_retry(context, 'end1', 'check'),
-        on_success_callback=lambda context: send_email_on_success(context, 'end1', 'check'),
+        on_success_callback=lambda context: send_email_on_retry(context, 'end1', 'check'),
     )
 
     check
